669a078ca93da42f07c8f60deb9ac472dbfc308f
jumpie <you@example.com>
2022-04-18 10:01:32 +0200

    Refactor code

--- jump app client/main.py
+++ jump app client/main.py
@@ -27,20 +27,30 @@ def getlink(appname,name):
             return applink[str(key)]
 check_settings()
 
+#make Settings Feather to json
 settings_df = pd.read_feather("./data/settings.feather").to_json()
 settings_json = json.loads(settings_df)
+
+#Get OS
 OS = settings_json["OS"]
 OS = OS["0"]
+
+#Update Package-list
 rasp = get(f"{server_ip}/db/json").content.decode()
 pd.read_json(rasp).to_feather("./data/rasp.feather")
+
+#get name from Package in next this work with args
+
 name = input("App Name: ")
+
+#some more Variables
 loaded_rasp = json.loads(rasp)
 appname = loaded_rasp["appname"]
 applink = loaded_rasp["applink"]
 link = getlink(appname,name)
-print(link)
 filename = basename(link)
 x = get(link).content
+#Download and unpack Package
 open(f"./temp/{filename}","wb").write(x)
 downloaded = tarfile.open(f"./temp/{filename}","r:gz")
 downloaded.extractall()
